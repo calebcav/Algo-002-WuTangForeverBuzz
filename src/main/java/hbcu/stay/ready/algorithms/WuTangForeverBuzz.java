d00265841fd201ad070c02e44d664c62d175bbb5
Caleb <cavinesscaleb@gmail.com>
2019-07-29 21:28:59 -0400

    Done

--- src/main/java/hbcu/stay/ready/algorithms/WuTangForeverBuzz.java
+++ src/main/java/hbcu/stay/ready/algorithms/WuTangForeverBuzz.java
@@ -3,6 +3,23 @@ package hbcu.stay.ready.algorithms;
 public class WuTangForeverBuzz {
 
     public String wuTangClan(){
-        return null;
+
+        String numbers = "";
+        for(int i = 1; i <= 15; i ++){
+            if((i % 3 == 0) && (i % 5 == 0)){
+                numbers += "WuTang Forever";
+            }
+            else if (i % 3 == 0){
+                numbers += "Wu\n";
+            }
+            else if (i % 5 == 0){
+                numbers += "Tang\n";
+            }
+            else{
+                numbers += i + "\n";
+            }
+        }
+
+        return numbers;
     }
 }
